3f19d332002bfff33a531e2af4cceb0cbf863609
Adrien Logut <newsadrien@gmail.com>
2023-01-08 04:21:40 -0500

    Add image view support in the swap chain

--- src/private/swapChain.cpp
+++ src/private/swapChain.cpp
@@ -19,11 +19,15 @@ SwapChain::SwapChain(VkDevice device, VkPhysicalDevice physicalDevice, VkSurface
     SwapChainSupportDetails swapChainSupport;
     FillSwapChainSupportDetails(physicalDevice, surface, swapChainSupport);
 
+    // Add some logic to select some settings on the swap chain
     SelectSwapSurfaceFormat(swapChainSupport);
     SelectSwapPresentMode(swapChainSupport);
     SelectSwapChainExtent(swapChainSupport, window);
 
+    // To avoid waiting for the device, we allow the swap chain to process 1 more image than the minimum.
     uint32_t imageCount = swapChainSupport.capabilities.minImageCount + 1;
+
+    // Also making sure we don't go over the max number of images supported.
     if (swapChainSupport.capabilities.maxImageCount > 0 && imageCount > swapChainSupport.capabilities.maxImageCount)
         imageCount = swapChainSupport.capabilities.maxImageCount;
 
@@ -57,6 +61,8 @@ SwapChain::SwapChain(VkDevice device, VkPhysicalDevice physicalDevice, VkSurface
     createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
     createInfo.presentMode = m_presentMode;
     createInfo.clipped = VK_TRUE;
+
+    // In case of an old swap chain, we pass it there.
     createInfo.oldSwapchain = oldSwapChain;
 
     if (vkCreateSwapchainKHR(device, &createInfo, nullptr, &m_swapChain) != VK_SUCCESS)
@@ -66,12 +72,54 @@ SwapChain::SwapChain(VkDevice device, VkPhysicalDevice physicalDevice, VkSurface
         return;
     }
 
-    vkGetSwapchainImagesKHR(device, m_swapChain, &imageCount, nullptr);
-    m_images.resize(imageCount);
-    vkGetSwapchainImagesKHR(device, m_swapChain, &imageCount, m_images.data());
+    InitImages(imageCount);
 }
 
-SwapChain::~SwapChain() { vkDestroySwapchainKHR(m_deviceCache, m_swapChain, nullptr); }
+SwapChain::~SwapChain()
+{
+    for (VkImageView imageView : m_imageViews)
+    {
+        vkDestroyImageView(m_deviceCache, imageView, nullptr);
+    }
+
+    vkDestroySwapchainKHR(m_deviceCache, m_swapChain, nullptr);
+}
+
+void SwapChain::InitImages(uint32_t imageCount)
+{
+    // First resize the image vector, and gather it from the device
+    vkGetSwapchainImagesKHR(m_deviceCache, m_swapChain, &imageCount, nullptr);
+    m_images.resize(imageCount);
+    vkGetSwapchainImagesKHR(m_deviceCache, m_swapChain, &imageCount, m_images.data());
+
+    // Also resize the image view vector
+    m_imageViews.resize(imageCount);
+
+    // And for each image, create its associated view
+    for (uint32_t i = 0; i < imageCount; ++i)
+    {
+        VkImageViewCreateInfo imageViewCreateInfo{};
+        imageViewCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+        imageViewCreateInfo.image = m_images[i];
+        imageViewCreateInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+        imageViewCreateInfo.format = m_surfaceFormat.format;
+        imageViewCreateInfo.components = {VK_COMPONENT_SWIZZLE_IDENTITY, VK_COMPONENT_SWIZZLE_IDENTITY,
+                                          VK_COMPONENT_SWIZZLE_IDENTITY, VK_COMPONENT_SWIZZLE_IDENTITY};
+
+        imageViewCreateInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        imageViewCreateInfo.subresourceRange.baseMipLevel = 0;
+        imageViewCreateInfo.subresourceRange.levelCount = 1;
+        imageViewCreateInfo.subresourceRange.baseArrayLayer = 0;
+        imageViewCreateInfo.subresourceRange.layerCount = 1;
+
+        if (vkCreateImageView(m_deviceCache, &imageViewCreateInfo, nullptr, &m_imageViews[i]) != VK_SUCCESS)
+        {
+            std::cout << "Failed to create image view number " << i << std::endl;
+            m_imageViews.resize(0);
+            return;
+        }
+    }
+}
 
 void SwapChain::FillSwapChainSupportDetails(VkPhysicalDevice device, VkSurfaceKHR surface,
                                             SwapChainSupportDetails& outDetails)
--- src/public/swapChain.h
+++ src/public/swapChain.h
@@ -22,10 +22,11 @@ public:
               VkSwapchainKHR oldSwapChain = VK_NULL_HANDLE);
     ~SwapChain();
 
-    bool IsValid() const { return m_swapChain != VK_NULL_HANDLE; }
+    bool IsValid() const { return m_swapChain != VK_NULL_HANDLE && !m_imageViews.empty(); }
 
     VkSwapchainKHR GetSwapChain() const { return m_swapChain; }
     std::vector<VkImage>& GetImages() { return m_images; }
+    std::vector<VkImageView>& GetImageViews() { return m_imageViews; }
     VkFormat GetFormat() const { return m_surfaceFormat.format; }
     VkExtent2D GetExtent() const { return m_extent; }
 
@@ -37,9 +38,12 @@ private:
     void SelectSwapPresentMode(const SwapChainSupportDetails& details);
     void SelectSwapChainExtent(const SwapChainSupportDetails& details, GLFWwindow* window);
 
+    void InitImages(uint32_t imageCount);
+
     VkDevice m_deviceCache = VK_NULL_HANDLE;
     VkSwapchainKHR m_swapChain = VK_NULL_HANDLE;
     std::vector<VkImage> m_images;
+    std::vector<VkImageView> m_imageViews;
     VkSurfaceFormatKHR m_surfaceFormat;
     VkPresentModeKHR m_presentMode;
     VkExtent2D m_extent;
